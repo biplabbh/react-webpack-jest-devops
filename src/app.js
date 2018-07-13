ee80bbc17dd6480a9826fab460023641db3517fe
Bhattacharya <Biplab.Bhattacharya@absa.co.za>
2018-07-13 10:40:17 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Welcome from './views/welcome';
+
+ReactDOM.render(
+  <Welcome name="World" />,
+  document.getElementById('root')
+);
